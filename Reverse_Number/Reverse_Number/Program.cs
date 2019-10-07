1912a82e38661e835e280de9c6defa8b03e8e617
Steven Scott <stevescott2653@gmail.com>
2019-10-07 14:29:50 -0400

    Add files via upload

---
+++ Reverse_Number/Reverse_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reverse Number: "+ reverse);
+        }
+    }
+}
